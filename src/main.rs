9e81d707c74e519626df2e4e592034428c86926c
gnarus-g <g.avet.gnaritas@gmail.com>
2022-04-11 23:46:21 -0400

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::env::args;
 use nchooserust::*;
+use std::env::args;
 
 fn main() {
     let args: Vec<String> = args().collect();
 
-    let n = args.get(1).map(|s| s.parse().unwrap_or(0)).unwrap_or(0);
-    let k = args.get(2).map(|s| s.parse().unwrap_or(0)).unwrap_or(0);
+    let n = args
+        .get(1)
+        .expect("Need to enter the value for n.")
+        .parse()
+        .expect("Couldn't parse number n from the arguments given.");
+
+    let k = args
+        .get(2)
+        .map(|s| {
+            s.parse()
+                .expect("Couldn't parse number k from the arguments given.")
+        })
+        .unwrap_or_default();
 
     println!("{:?}", n_choose_k(n, k));
 }
