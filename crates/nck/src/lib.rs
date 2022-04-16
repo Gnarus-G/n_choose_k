60e9a50c768045e914e01fe1221bf6d8f5b37e57
gnarus-g <g.avet.gnaritas@gmail.com>
2022-04-16 11:05:17 -0400

    enhanced to support cardinality higher than 34

--- crates/nck/src/lib.rs
+++ crates/nck/src/lib.rs
@@ -1,17 +1,32 @@
-pub fn n_choose_k(n: u128, k: u128) -> u128 {
+use num::BigUint;
+
+pub fn n_choose_k(n: u128, k: u128) -> BigUint {
     match k {
-        0 => 1,
-        1 => n,
-        k if k == n => 1,
-        k if n - k == 1 => n,
-        _ => fact(n) / fact(k) / fact(n - k),
+        0 => bigu128(1),
+        1 => bigu128(n),
+        k if k == n => bigu128(1),
+        k if n - k == 1 => bigu128(n),
+        _ => fact(bigu128(n)) / fact(bigu128(k)) / fact(bigu128(n - k)),
     }
 }
 
-fn fact(n: u128) -> u128 {
-    match n {
-        0 | 1 => 1,
-        2 => 2,
-        _ => n * fact(n - 1),
-    }
+fn fact(n: BigUint) -> BigUint {
+    let is_zero_or_1 = bigu128(0) == n || bigu128(1) == n;
+
+    return if is_zero_or_1 {
+        bigu128(1)
+    } else {
+        &n * fact(&n - bigu128(1))
+    };
+}
+
+#[inline]
+pub fn bigu128(i: u128) -> BigUint {
+    BigUint::from(i)
+}
+
+#[test]
+#[cfg(test)]
+fn test_fact_no_crash() {
+    fact(bigu128(35)); //fact(35u128) would cause overflow and crash;
 }
--- crates/nck/tests/lib.rs
+++ crates/nck/tests/lib.rs
@@ -1,21 +1,26 @@
 #[cfg(test)]
 mod tests {
-    use nck::n_choose_k;
+    use nck::*;
 
     #[test]
     fn test_n_choose_k() {
-        assert_eq!(n_choose_k(3, 0), 1);
-        assert_eq!(n_choose_k(3, 1), 3);
-        assert_eq!(n_choose_k(3, 2), 3);
-        assert_eq!(n_choose_k(30, 3), 4060)
+        assert_eq!(n_choose_k(3, 0), bigu128(1));
+        assert_eq!(n_choose_k(3, 1), bigu128(3));
+        assert_eq!(n_choose_k(3, 2), bigu128(3));
+        assert_eq!(n_choose_k(30, 3), bigu128(4060))
     }
 
     #[test]
     fn test_no_overflow_on_trivial_input() {
-        assert_eq!(n_choose_k(50, 1), 50);
-        assert_eq!(n_choose_k(90, 1), 90);
-        assert_eq!(n_choose_k(35, 34), 35);
-        assert_eq!(n_choose_k(10000, 0), 1);
-        assert_eq!(n_choose_k(10000, 10000), 1);
+        assert_eq!(n_choose_k(50, 1), bigu128(50));
+        assert_eq!(n_choose_k(90, 1), bigu128(90));
+        assert_eq!(n_choose_k(35, 34), bigu128(35));
+        assert_eq!(n_choose_k(10000, 0), bigu128(1));
+        assert_eq!(n_choose_k(10000, 10000), bigu128(1));
+    }
+
+    #[test]
+    fn test_no_overflow() {
+        assert_eq!(n_choose_k(35, 2), bigu128(595));
     }
 }
