90280ee81f40699ff21240eca5378d08cfa3c32b
gnarus-g <g.avet.gnaritas@gmail.com>
2022-04-17 11:31:29 -0400

    bad recursion, bad!

--- crates/factorial/src/lib.rs
+++ crates/factorial/src/lib.rs
@@ -39,16 +39,6 @@ pub fn fact_iter(n: BigUint) -> BigUint {
     range_inclusive(big(1), n).product()
 }
 
-pub fn fact_recur(n: BigUint) -> BigUint {
-    let is_zero_or_1 = big(0) == n || big(1) == n;
-
-    return if is_zero_or_1 {
-        big(1)
-    } else {
-        &n * fact_recur(&n - big(1))
-    };
-}
-
 #[inline(always)]
 fn big(i: u128) -> BigUint {
     BigUint::from(i)
@@ -56,15 +46,12 @@ fn big(i: u128) -> BigUint {
 
 #[cfg(test)]
 mod tests {
-    use std::time::{Instant, Duration};
+    use std::time::{Duration, Instant};
 
     use crate::*;
 
     #[test]
     fn test_fact_simple() {
-        assert_eq!(fact_recur(big(5)), big(120));
-        assert_eq!(fact_recur(big(3)), big(6));
-
         assert_eq!(fact_iter(big(5)), big(120));
         assert_eq!(fact_iter(big(3)), big(6));
     }
@@ -82,10 +69,5 @@ mod tests {
         cf.get(big(25000));
         cf.get(big(25000));
         assert!(Instant::now().duration_since(then) < Duration::from_secs(2));
-    }
-
-    #[test]
-    fn test_fact_iter() {
-        fact_iter(big(99999)); //fact_recur(big(99999)) would cause stack overflow and crash, when implemented with recursion
     }
 }
