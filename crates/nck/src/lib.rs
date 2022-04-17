d2f0db4211cd5531cae2bac2c416db1d13316146
gnarus-g <g.avet.gnaritas@gmail.com>
2022-04-17 11:36:32 -0400

    caching factorials during calculation

--- crates/nck/src/lib.rs
+++ crates/nck/src/lib.rs
@@ -1,18 +1,16 @@
+use factorial::CachedFactorials;
 use num::BigUint;
 
 pub fn n_choose_k(n: u128, k: u128) -> BigUint {
+    let mut factorials = CachedFactorials::new();
+
     match k {
         0 => big(1),
         1 => big(n),
         k if k == n => big(1),
         k if n - k == 1 => big(n),
-        _ => fact(big(n)) / fact(big(k)) / fact(big(n - k)),
+        _ => factorials.get(big(n)) / factorials.get(big(k)) / factorials.get(big(n - k)),
     }
-}
-
-#[inline]
-fn fact(n: BigUint) -> BigUint {
-    factorial::fact_iter(n)
 }
 
 #[inline]
